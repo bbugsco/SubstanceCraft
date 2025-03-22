055e0fa7737212b4fa6339a8051c503e3d197ab0
bbugsco <bbugsco@gmail.com>
2025-03-22 09:51:49 -0600

    initial commit

---
+++ src/main/java/com/github/bbugsco/substancecraft/SubstanceCraft.java
@@ -0,0 +1,16 @@
+package com.github.bbugsco.substancecraft;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SubstanceCraft implements ModInitializer {
+
+    public static final String MOD_ID = "substancecraft";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Initializing SubstanceCraft");
+    }
+}
---
+++ src/main/java/com/github/bbugsco/substancecraft/client/SubstanceCraftClient.java
@@ -0,0 +1,13 @@
+package com.github.bbugsco.substancecraft.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class SubstanceCraftClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
